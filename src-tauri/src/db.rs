12dc4afaac28754d1735b26c230247000aec1d05
_JXc <hsvbuh12@gmail.com>
2023-10-03 22:17:19 +0200

    1.0.0 - database accessors

---
+++ src-tauri/src/db.rs
@@ -0,0 +1,85 @@
+use sqlite;
+use sqlite::{Connection, open, Row};
+use std::collections::VecDeque;
+
+static DB_PATH: &str = "C:\\Users\\Jxson\\jutils.ui\\data.db";
+
+pub struct Database
+{
+    pub(self) batch_trans: bool,
+    pub(self) rows_updated: u32,
+    pub(self) db_location: String,
+
+    pub(self) command_prio: usize,
+    pub(self) command_queue: VecDeque<String>
+
+}
+
+impl Database
+{
+    pub(crate) fn new(path_to_db: String) -> Self {
+        Self {
+            batch_trans: false,
+            rows_updated: 0,
+            db_location: (path_to_db.to_owned()).to_owned(),
+            command_queue: VecDeque::new(),
+            command_prio: 0
+        }
+    }
+
+    /**
+    * Vulnerable to sql-injections...
+    */
+    pub fn query(&self, query: &str) -> Option<sqlite::Result<Row>>
+    {
+        let connection: Connection = open(":memory:").unwrap();
+        let mut prepared_statement = connection.prepare(query).unwrap();
+
+        prepared_statement.iter().next()
+
+    }
+
+    pub fn execute(&mut self, query: &str) -> u32
+    {
+        if self.is_using_batch_trans() == true
+        {
+            self.command_queue.insert(self.command_prio,query.to_owned());
+            self.command_prio+=1;
+            return 0
+        }
+        let connection: Connection = open(DB_PATH).unwrap();
+        match connection.execute(query) {
+            Ok(_) => 1,
+            Err(_) => 0,
+        }
+    }
+
+    pub fn use_batch(&mut self, batching: bool)
+    {
+       self.batch_trans = batching
+    }
+
+
+
+    pub fn commit(&mut self) -> u32
+    {
+        let connection: Connection = open(DB_PATH).unwrap();
+
+        self.command_queue.retain(| current| {
+            match connection.execute(current) {
+                Ok(_) => self.rows_updated+=1,
+                Err(_) => ()
+            };
+            true
+        });
+
+        self.rows_updated
+    }
+
+    pub fn get_rows_updated(&self) -> u32 {
+        self.rows_updated
+    }
+    fn is_using_batch_trans(&self) -> bool {
+        self.batch_trans
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod api;
-mod files;
+mod db;
 
+use std::path::PathBuf;
 use rand::Rng;
-use api::{get_user_dir};
-use files::config::Config;
+use dirs;
+use crate::db::Database;
+
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -22,15 +23,38 @@ fn randommessage() -> String {
 
 fn main() {
 
-    let conf = Config::new("jnotes");
 
+    let default_path: String = {
+        let user_home = match dirs::home_dir() {
+            Some(path) => Some(path.to_string_lossy().into_owned()),
+            None => Some(String::from("./jutils.ui"))
+        };
+        let mut tmp_path_buf = PathBuf::new();
+        tmp_path_buf.push(user_home.unwrap());
+        tmp_path_buf.push("jutils.ui");
+        tmp_path_buf.to_string_lossy().into_owned()
+    };
+
+    std::fs::create_dir_all(PathBuf::from(default_path.clone()).as_path())
+        .expect("Unable to create parent directories!");
+
+    let mut database_ins: Database = Database::new((default_path + "\\data.db"));
+
+    database_ins.use_batch(true);
+    database_ins.execute(String::from("CREATE TABLE u_settings(id VARCHAR(255) NOT NULL, value VARCHAR(255), PRIMARY KEY (id));").as_str());
+    database_ins.execute(format!("INSERT INTO u_settings(id, value) VALUES ({0}, {1});", "\"notes_save_location\"", "\"{HOME-DIR}/notes/\"").as_str());
+    database_ins.commit();
+    database_ins.use_batch(false);
+
+    println!("{} overall rows updated", database_ins.get_rows_updated());
+
+    println!("Application started!");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             greet,
-            randommessage,
-            get_user_dir,
-            conf.dir_exists
+            randommessage
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
 }
